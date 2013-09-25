561e58b790d0415f368c171f64d4e0453823a0eb
James Stevenson <james@stev.org>
2013-09-25 11:58:36 +0100

    Removed Unused code

--- include/libclientserver/Time.h
+++ include/libclientserver/Time.h
@@ -2,8 +2,6 @@
 class Time
 {
 	public:
-		static boost::system_time CalcTimeout(const struct timespec *Timeout);
-		static boost::system_time CalcTimeout(const struct timeval *Timeout);
 		
 		static void Sleep(const struct timespec *Timeout);
 };
--- src/Time.cpp
+++ src/Time.cpp
@@ -1,22 +1,6 @@
 
 #include <libclientserver.h>
 
-boost::system_time Time::CalcTimeout(const struct timespec *Timeout)
-{
-	boost::system_time timeout = boost::get_system_time();
-	timeout += boost::posix_time::seconds(Timeout->tv_sec);
-	timeout += boost::posix_time::microseconds(Timeout->tv_nsec / 1000);
-	return timeout;
-}
-
-boost::system_time Time::CalcTimeout(const struct timeval *Timeout)
-{
-	boost::system_time timeout = boost::get_system_time();
-	timeout += boost::posix_time::seconds(Timeout->tv_sec);
-	timeout += boost::posix_time::microseconds(Timeout->tv_usec);
-	return timeout;
-}
-
 void Time::Sleep(const struct timespec *Timeout)
 {
 	struct timespec rem;
