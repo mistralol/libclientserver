2f23a699d11d686edb2fee2969f6706424c80c38
James Stevenson <james@stev.org>
2013-10-30 11:59:43 +0000

    Added Docs to Mutex.cpp Mutex.h

--- include/libclientserver/Mutex.h
+++ include/libclientserver/Mutex.h
@@ -1,5 +1,14 @@
 
-
+/**
+ * @class Mutex
+ * @brief Class To support Mutex and Mutex releated operations
+ *
+ * Implementes a Mutex. This version it really a wrapper around the pthread mutex
+ * and condition operations. The wrapper exists so that profiling / benchmark and tracing
+ * can be added if it is required to support debugging later.
+ *
+ * Currently the class will do basic checking if DEBUG is defined. eg trying to unlock a mutex that is not locked.
+ */
 class Mutex {
 	
 	public:
@@ -12,7 +21,7 @@ class Mutex {
 		void Unlock();
 
 		void Wait();
-		int Wait(const struct timespec *abstime);
+		int Wait(const struct timespec *Timeout);
 		void WakeUp();
 		void WakeUpAll();
 
--- src/Mutex.cpp
+++ src/Mutex.cpp
@@ -1,6 +1,11 @@
 
 #include <libclientserver.h>
 
+/**
+ * Mutex
+ *
+ * Constructor for Mutex. If it fails it will call abort since these functions should *NEVER* fail.
+ */
 Mutex::Mutex() {
 	if (pthread_mutex_init(&m_mutex, NULL) != 0)
 		abort();
@@ -11,9 +16,19 @@ Mutex::Mutex() {
 	m_locked = false;
 }
 
+/**
+ * ~Mutex
+ *
+ * Destructor for Mutex. This should never fail. If it does it will call abort.
+ *
+ * If DEBUG is defined it will check if the Mutex is still locked.
+ * If it is it will call abort as it indicates a software bug.
+ */
 Mutex::~Mutex() {
+#ifdef DEBUG
 	if (m_locked)
 		abort(); //Attempts to free mutex that is locked
+#endif
 
 	if (pthread_cond_destroy(&m_cond) != 0)
 		abort();
@@ -21,13 +36,26 @@ Mutex::~Mutex() {
 	if (pthread_mutex_destroy(&m_mutex) != 0)
 		abort();
 }
-		
+
+/**
+ * Lock
+ *
+ * Lock the Mutex. This function should never fail. Though it may hang forever
+ */
 void Mutex::Lock() {
 	if (pthread_mutex_lock(&m_mutex) < 0)
 		abort(); //Could not lock mutex
 	m_locked = true;
 }
 
+/**
+ * TryLock
+ * @return Will return 0 if the lock is successful. Otherwise -errno
+ *
+ * Will attempt to take a lock of the mutex.
+ * If the mutex is already locked by another thread then the function will fail and return -errno
+ * The error return should be -EBUSY for this condition. Anything else should be treated as a bug.
+ */
 int Mutex::TryLock() {
 	int ret = pthread_mutex_lock(&m_mutex);
 	if (ret < 0)
@@ -36,6 +64,16 @@ int Mutex::TryLock() {
 	return 0;
 }
 
+/**
+ * TimedLock
+ * @param[in] Timeout The length of time to wait for a timeout
+ * @return Will return 0 if the lock is successful. Otherwise -errno
+ *
+ * Will attempt to take a lock of the mutex.
+ * If the mutex is already locked by another thread then the function will sleep for the Timeout period.
+ * The error returned should be -ETIMEOUT if the Timeout expires before the lock is aquired.
+ * Any other error should be treated as asoftware bug
+ */
 int Mutex::TimedLock(const struct timespec *Timeout)
 {
 	int ret = pthread_mutex_timedlock(&m_mutex, Timeout);
@@ -47,12 +85,29 @@ int Mutex::TimedLock(const struct timespec *Timeout)
 	return -errno;
 }
 
+/**
+ * Unlock
+ *
+ * Unlocks a locked mutex
+ * This function should never fail. If it does abort will be called.
+ * Abort could be called if the caller attempt to unlock a mutex that is not locked.
+ * Which is a software bug on the caller side.
+ *
+ */
 void Mutex::Unlock() {
 	if (pthread_mutex_unlock(&m_mutex) < 0)
 		abort(); //Could not unlock mutex
 	m_locked = false;
 }
 
+/**
+ * Wait
+ *
+ * Wait until another thread calls WakeUp or WakeUpAll
+ *
+ * The thread must already hold the lock before this function is called.
+ * If DEBUG is defined at compile time this will cause abort to be called if the Mutex is not locked.
+ */
 void Mutex::Wait() {
 #ifdef DEBUG
 	if (m_locked == false)
@@ -66,7 +121,20 @@ void Mutex::Wait() {
 	m_locked = true;
 }
 
-int Mutex::Wait(const struct timespec *abstime) {
+/**
+ * Wait
+ * @params[in] Timeout The length of time to wait until we return
+ * @return Return 0 on success. Or -errno
+ *
+ * Wait until another thread calls WakeUp or WakeUpAll. Or the Timeout expires. If the Timeout is reached -ETIMEOUT will be returned.
+ * 
+ * The thread must already hold the lock before this function is called.
+ * If DEBUG is defined at compile time this will cause abort to be called if the Mutex is not locked.
+ *
+ * Note: It is possbile for this function to return without WakeUp or WakeUpAll being returned.
+ *
+ */
+int Mutex::Wait(const struct timespec *Timeout) {
 #ifdef DEBUG
 	if (m_locked == false)
 		abort();
@@ -78,7 +146,7 @@ int Mutex::Wait(const struct timespec *abstime) {
 		abort();
 
 	Time::TimeValtoTimeSpec(&now, &ts1);
-	Time::Add(abstime, &ts1, &ts2);
+	Time::Add(Timeout, &ts1, &ts2);
 
 	m_locked = false;
 	int ret = pthread_cond_timedwait(&m_cond, &m_mutex, &ts2);
@@ -96,15 +164,29 @@ int Mutex::Wait(const struct timespec *abstime) {
 				break;
 		}
 	}
-	return ret;
+	return ret; /* Unreachable! */
 }
 
+/**
+ * WakeUp
+ * 
+ * WakeUp a single thread that is sleeping in WakeUp
+ *
+ * It is not required to have the lock when this function is called.
+ */
 void Mutex::WakeUp() {
 	int ret = pthread_cond_signal(&m_cond);
 	if (ret < 0)
 		abort(); //pthread_cond_signal failed
 }
 
+/**
+ * WakeUp
+ * 
+ * WakeUp a all threads that is sleeping in WakeUp
+ *
+ * It is not required to have the lock when this function is called.
+ */
 void Mutex::WakeUpAll() {
 	int ret = pthread_cond_broadcast(&m_cond);
 	if (ret < 0)
