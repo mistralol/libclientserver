77b280de9a0e8e74979abf42a52752264fbf8d4b
James Stevenson <james@stev.org>
2014-04-30 13:08:34 +0100

    Added TestServer

---
+++ testing/TestServer.cpp
@@ -0,0 +1,13 @@
+
+#include <libclientserver.h>
+
+#include <TestServer.h>
+
+
+int main(int argc, char **argv)
+{
+	TestServer TS;
+
+	return 0;
+}
+
---
+++ testing/TestServer.h
@@ -0,0 +1,114 @@
+
+class Handler : public IServerHandler
+{
+private:
+	bool m_Quit;
+
+public:
+	Handler()
+	{
+		m_Quit = false;
+	}
+
+	void OnPreNewConnection()
+	{
+		printf("PreNewConnection\n");
+	}
+
+	void OnPostNewConnection(IServerConnection *Connection)
+	{
+		printf("PostNewConnection\n");
+	}
+		
+	void OnDisconnect(IServerConnection *Connection)
+	{
+		printf("Disconnect\n");
+	}
+		
+	int OnRequest(IServerConnection *Connection, Request *request, Request *response)
+	{
+		std::list<std::string> lst = request->GetArgList();
+		std::stringstream ss;
+		std::string Command = request->GetCommand();
+
+		ss << "OnRequest: " << request->GetCommand() << " ";
+
+		for(std::list<std::string>::iterator it = lst.begin(); it != lst.end(); it++)
+		{
+			ss << *it << "=" << request->GetArg(*it) << " ";
+		}
+
+		printf("%s\n", ss.str().c_str());
+
+		if (Command == "PING")
+			return 0;
+
+		if (Command == "QUIT")
+		{
+			m_Quit = true;
+			return 0;
+		}
+
+		
+		return -ENOSYS;
+	}
+	
+	int OnCommand(IServerConnection *Connection, Request *request)
+	{
+		printf("OnCommand: %s\n", request->GetCommand().c_str());
+		return -ENOSYS;
+	}
+
+	void OnBadLine(IServerConnection *Connection, const std::string *line)
+	{
+		printf("Bad Line: %s\n", line->c_str());
+	}
+
+	void Wait()
+	{
+		while(m_Quit == false)
+		{
+			printf("Running\n");
+			sleep(1);
+		}
+	}
+
+};
+
+class TestServer : Thread
+{
+	public:
+		TestServer()
+		{
+			m_quit = false;
+			Start();
+		}
+
+		~TestServer()
+		{
+			m_quit = true;
+			Stop();
+		}
+
+		virtual void Run()
+		{
+			Handler SrvHandler;
+			ServerManager Manager(&SrvHandler);
+	
+			ServerUnix Unix("/tmp/TestServer");
+	
+			Manager.ServerAdd(&Unix);
+			while(m_quit == false)
+			{
+				sleep(1);
+			}
+
+			Manager.ServerRemove(&Unix);
+		}
+	private:
+		bool m_quit;
+};
+
+
+
+
