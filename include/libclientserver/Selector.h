35e09e5ba1563570d2daf78b7f2c932e0df07c7e
James Stevenson <james@stev.org>
2015-03-02 12:16:16 +0000

    Added more docs to Selector

--- include/libclientserver/Selector.h
+++ include/libclientserver/Selector.h
@@ -11,10 +11,16 @@
  * When a class is added it must return a valid file descriptor. If it does not the program will abort.
  *
  * After a class is added to the select engine its file descriptor must not change until the class is removed from the selector.
+ * so the correct way to change a file descriptor would be to Remove(this); ChangeFD, Add(this)
  *
  * Classes can be added / remove from within the interface calls. The permissions of what can be done are controlled by the const.
  *
  * It is safe to remove and delete and instance of the class from the DoRead, DoWrite, DoExcept, DoTimeout functions
+ *
+ * None of the interface calls should block. If a block should occur then the select engine will fail to process data flow for other file descriptors
+ *
+ * Every time one of DoRead, DoWrite, DoExcept, DoTimeout functions are called. The CanRead, CanWrite, CanExcept, CanTimeout
+ * functions will be checked after all of the event calsl are made.
  */
 
 class Selector : private Thread
