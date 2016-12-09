14d9fc7104449a3b40c99e81b3c31fb5b2e21e74
James Stevenson <james@stev.org>
2016-12-09 15:10:27 +0000

    Remove node client from project. Its being split to its own for npm reasons

--- node/test-install.js
+++
@@ -1,9 +0,0 @@
-
-var libclientserver = require("/usr/lib/node_modules/libclientserver");
-
-var Cli = new libclientserver.Client("/tmp/SimpleServer");
-Cli.Connect();
-
-
-
-console.log("Started!");
