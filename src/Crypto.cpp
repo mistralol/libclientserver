a0b4ee4c51af8dfcb1a478c07d5e7f3af5039a18
James Stevenson <james@stev.org>
2018-03-21 09:11:53 +0000

    Trim WhiteSpace

--- src/Crypto.cpp
+++ src/Crypto.cpp
@@ -8,11 +8,11 @@ std::string Crypto::SHA1Pass(const std::string pass, const std::string salt)
 {
 	std::stringstream ss;
 	unsigned char out[20];
-	
+
 	int ret = PKCS5_PBKDF2_HMAC_SHA1(pass.c_str(), 0, (unsigned char *) salt.c_str(), salt.size(), 1000, sizeof(out), out);
 	if (ret != 1)
 		abort();
-	
+
 	for(size_t i=0;i<sizeof(out);i++)
 	{
 		char tmp[3];
